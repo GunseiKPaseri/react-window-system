2855a4abd112c7f4122de770aa8ad81da3acb5d3
GunseiKPaseri <GunseiKPaseri@gmail.com>
2024-05-03 01:17:41 +0900

    add vite-plugin-biome

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import react from "@vitejs/plugin-react-swc";
 import pkg from "./package.json";
 import { visualizer } from "rollup-plugin-visualizer";
+import biomePlugin from "vite-plugin-biome"
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer()],
+  plugins: [react(), visualizer(), biomePlugin({
+    mode: "check",
+    files: "src/**/*.ts",
+  })],
   build: {
     lib: {
       entry: "src/index.ts",
